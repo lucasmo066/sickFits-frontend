c6400203cfe7f61a46eecc908f114f9778a1cb32
Lucas Moraes <lucasmo066@gmail.com>
2022-08-17 16:13:09 -0400

    added initial styling to header

---
+++ components/Header.js
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+import Nav from './Nav';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: red;
+  transform: skew(-7deg);
+  background: red;
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+export default function Header() {
+  return (
+    <header>
+      <div className="bar">
+        <Logo>
+          <Link href="/">Sick Fits</Link>
+        </Logo>
+      </div>
+      <div className="sub-bar">
+        <p>Search</p>
+        <Nav />
+      </div>
+    </header>
+  );
+}
